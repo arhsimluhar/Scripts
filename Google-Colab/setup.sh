5e7073c3a5329a66ec17c44a1733b40acca8abf3
rahul1809 <rahul.mishra2003@gmail.com>
2019-12-12 23:41:57 +0800

    installation of git and required pip packages and cleanup

--- Google-Colab/setup.sh
+++ Google-Colab/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #update and install required packages
-apt-get update -y && apt-get install python3 python3-dev python3-pip git -y && pip install -r requirements.txt
+apt-get update -y && apt-get install python3 python3-dev python3-pip git -y
 
 #clone the repository and process the script further
 git clone https://github.com/rahul1809/Scripts.git
